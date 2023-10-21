6ea13bb656dc08b7a475782da23d999609d18bc9
Faruk Aydin <farukaydin@outlook.com>
2023-10-21 12:16:30 +0300

    update workflows

--- dist/index.js
+++ dist/index.js
@@ -25641,54 +25641,127 @@ const core = __importStar(__nccwpck_require__(2186));
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
-    try {
-        const token = core.getInput('token');
-        const projectName = core.getInput('project');
-        const accountId = core.getInput('account-id');
-        const domain = core.getInput('domain');
-        const operation = core.getInput('operation');
-        if (operation === 'create') {
-            core.debug(`Creating domain ${domain} for project ${projectName}`);
-            const res = await fetch(`https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}/domains`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`
-                },
-                body: JSON.stringify({
-                    name: domain
-                })
-            }).then(res => res.json());
-            core.debug(JSON.stringify(res));
-            if (!res.success) {
-                throw new Error('Failed');
-            }
-        }
-        else if (operation === 'delete') {
-            const res = await fetch(`https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}/domains/${domain}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`
-                }
-            }).then(res => res.json());
-            core.debug(JSON.stringify(res));
-            if (!res.success) {
-                throw new Error('Failed');
-            }
-        }
-        else {
-            throw new Error('Invalid operation');
-        }
-        // Set outputs for other workflow steps to use
+    const token = core.getInput('token');
+    const projectName = core.getInput('project');
+    const accountId = core.getInput('account-id');
+    const name = core.getInput('name');
+    const operation = core.getInput('operation');
+    const zoneId = core.getInput('zoneId');
+    const content = core.getInput('content');
+    if (operation === 'create') {
+        await createRecord({ content, token, zoneId, name });
+        await createCustomDomain({ accountId, name, projectName, token });
     }
-    catch (error) {
-        // Fail the workflow run if an error occurs
-        if (error instanceof Error)
-            core.setFailed(error.message);
+    else if (operation === 'delete') {
+        const id = await getCurrentRecordId({ name, token, zoneId });
+        await deleteRecord({ id, token, zoneId });
+        await deleteCustomDomain({ accountId, name, projectName, token });
     }
+    // Set outputs for other workflow steps to use
 }
 exports.run = run;
+const getCurrentRecordId = async ({ name, token, zoneId }) => {
+    try {
+        const res = await fetch(`https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        }).then(res => res.json());
+        core.debug(JSON.stringify(res));
+        if (!res.success) {
+            throw new Error('Failed');
+        }
+        const record = res.find((x) => x.name === name);
+        return record?.id;
+    }
+    catch {
+        throw new Error('failed');
+    }
+};
+const createRecord = async ({ content, token, zoneId, name }) => {
+    try {
+        const dnsRes = await fetch(`https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                type: 'CNAME',
+                name,
+                content,
+                proxied: true
+            })
+        }).then(res => res.json());
+        core.debug(JSON.stringify(dnsRes));
+        if (!dnsRes.success) {
+            throw new Error('Failed');
+        }
+    }
+    catch {
+        throw Error('failed');
+    }
+};
+const createCustomDomain = async ({ accountId, name, projectName, token }) => {
+    try {
+        core.debug(`Creating domain ${name} for project ${projectName}`);
+        const res = await fetch(`https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}/domains`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                name: name
+            })
+        }).then(res => res.json());
+        core.debug(JSON.stringify(res));
+        if (!res.success) {
+            throw new Error('Failed');
+        }
+    }
+    catch {
+        throw Error('failed');
+    }
+};
+const deleteCustomDomain = async ({ accountId, name, projectName, token }) => {
+    try {
+        const res = await fetch(`https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}/domains/${name}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        }).then(res => res.json());
+        core.debug(JSON.stringify(res));
+        if (!res.success) {
+            throw new Error('Failed');
+        }
+    }
+    catch {
+        throw Error('failed');
+    }
+};
+const deleteRecord = async ({ id, token, zoneId }) => {
+    try {
+        const res = await fetch(`https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        }).then(res => res.json());
+        core.debug(JSON.stringify(res));
+        if (!res.success) {
+            throw new Error('Failed');
+        }
+    }
+    catch {
+        throw Error('failed');
+    }
+};
 
 
 /***/ }),
--- src/main.ts
+++ src/main.ts
@@ -6,55 +6,197 @@ import * as github from '@actions/github'
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
+  const token: string = core.getInput('token')
+  const projectName: string = core.getInput('project')
+  const accountId: string = core.getInput('account-id')
+  const name: string = core.getInput('name')
+  const operation: string = core.getInput('operation')
+  const zoneId: string = core.getInput('zoneId')
+  const content: string = core.getInput('content')
+
+  if (operation === 'create') {
+    await createRecord({ content, token, zoneId, name })
+    await createCustomDomain({ accountId, name, projectName, token })
+  } else if (operation === 'delete') {
+    const id = await getCurrentRecordId({ name, token, zoneId })
+    await deleteRecord({ id, token, zoneId })
+    await deleteCustomDomain({ accountId, name, projectName, token })
+  }
+
+  // Set outputs for other workflow steps to use
+}
+
+const getCurrentRecordId = async ({
+  name,
+  token,
+  zoneId
+}: {
+  name: string
+  zoneId: string
+  token: string
+}) => {
   try {
-    const token: string = core.getInput('token')
-    const projectName: string = core.getInput('project')
-    const accountId: string = core.getInput('account-id')
-    const domain: string = core.getInput('domain')
-    const operation: string = core.getInput('operation')
-
-    if (operation === 'create') {
-      core.debug(`Creating domain ${domain} for project ${projectName}`)
-      const res = await fetch(
-        `https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}/domains`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
-          },
-          body: JSON.stringify({
-            name: domain
-          })
+    const res = await fetch(
+      `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
         }
-      ).then(res => res.json())
-      core.debug(JSON.stringify(res))
-      if (!res.success) {
-        throw new Error('Failed')
       }
-    } else if (operation === 'delete') {
-      const res = await fetch(
-        `https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}/domains/${domain}`,
-        {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
-          }
-        }
-      ).then(res => res.json())
-      core.debug(JSON.stringify(res))
+    ).then(res => res.json())
 
-      if (!res.success) {
-        throw new Error('Failed')
-      }
-    } else {
-      throw new Error('Invalid operation')
+    core.debug(JSON.stringify(res))
+
+    if (!res.success) {
+      throw new Error('Failed')
     }
 
-    // Set outputs for other workflow steps to use
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    const record = res.find((x: any) => x.name === name)
+
+    return record?.id
+  } catch {
+    throw new Error('failed')
+  }
+}
+
+const createRecord = async ({
+  content,
+  token,
+  zoneId,
+  name
+}: {
+  zoneId: string
+  content: string
+  token: string
+  name: string
+}) => {
+  try {
+    const dnsRes = await fetch(
+      `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          type: 'CNAME',
+          name,
+          content,
+          proxied: true
+        })
+      }
+    ).then(res => res.json())
+
+    core.debug(JSON.stringify(dnsRes))
+
+    if (!dnsRes.success) {
+      throw new Error('Failed')
+    }
+  } catch {
+    throw Error('failed')
+  }
+}
+
+const createCustomDomain = async ({
+  accountId,
+  name,
+  projectName,
+  token
+}: {
+  projectName: string
+  accountId: string
+  token: string
+  name: string
+}) => {
+  try {
+    core.debug(`Creating domain ${name} for project ${projectName}`)
+    const res = await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}/domains`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          name: name
+        })
+      }
+    ).then(res => res.json())
+
+    core.debug(JSON.stringify(res))
+
+    if (!res.success) {
+      throw new Error('Failed')
+    }
+  } catch {
+    throw Error('failed')
+  }
+}
+
+const deleteCustomDomain = async ({
+  accountId,
+  name,
+  projectName,
+  token
+}: {
+  accountId: string
+  projectName: string
+  name: string
+  token: string
+}) => {
+  try {
+    const res = await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}/domains/${name}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+    ).then(res => res.json())
+
+    core.debug(JSON.stringify(res))
+
+    if (!res.success) {
+      throw new Error('Failed')
+    }
+  } catch {
+    throw Error('failed')
+  }
+}
+
+const deleteRecord = async ({
+  id,
+  token,
+  zoneId
+}: {
+  token: string
+  zoneId: string
+  id: string
+}) => {
+  try {
+    const res = await fetch(
+      `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+    ).then(res => res.json())
+
+    core.debug(JSON.stringify(res))
+
+    if (!res.success) {
+      throw new Error('Failed')
+    }
+  } catch {
+    throw Error('failed')
   }
 }
